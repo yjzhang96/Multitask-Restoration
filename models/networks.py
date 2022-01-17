c127c9a11a40254a6676c61bda07e09b6186816a
yjzhang96 <4591203423@qq.com>
2022-01-17 14:53:57 +1100

    submodule

--- models/networks.py
+++ models/networks.py
@@ -9,7 +9,6 @@ import functools
 from .functions import ReverseLayerF
 from .UNet_discriminator import UNetDiscriminator
 # from .DCN_v2.modules.modulated_deform_conv import ModulatedDeformConv_blur
-# from .bdcn import bdcn
 from .MPRNet import MPRNet
 
 def _get_orthogonal_init_weights(weights):
@@ -156,10 +155,6 @@ def define_net_offset(config):
     model_d = init_net(model_d,gpu_ids=config['gpu'])
     return model_d
 
-def define_BDCN(config):
-    net = bdcn.BDCN()
-    net = init_net(net,gpu_ids=config['gpu'])
-    return net
 
 def define_offset_quad(input_nc, nf, n_offset, norm='batch', gpu_ids=[]):
     net_offset = None
@@ -205,21 +200,6 @@ def define_blur_layer(input_nc=3, output_nc=3, n_offset=1,gpu_ids=[]):
     net_blur = init_net(net_blur, gpu_ids=gpu_ids,initialize_weights=False)
     return net_blur
 
-def define_F(input_nc, netF, norm='batch', use_dropout=False, init_type='xavier', init_gain=0.02, no_antialias=False, gpu_ids=[], opt=None):
-    # if netF == 'global_pool':
-    #     net = PoolingF()
-    if netF == 'reshape':
-        net = ReshapeF()
-    elif netF == 'sample':
-        net = PatchSampleF(use_mlp=False, init_type=init_type, init_gain=init_gain, gpu_ids=gpu_ids, nc=opt.netF_nc)
-    elif netF == 'mlp_sample':
-        net = PatchSampleF(use_mlp=True, init_type=init_type, init_gain=init_gain, gpu_ids=gpu_ids, nc=opt.netF_nc)
-    # elif netF == 'strided_conv':
-    #     net = StridedConvF(init_type=init_type, init_gain=init_gain, gpu_ids=gpu_ids)
-    else:
-        raise NotImplementedError('projection model name [%s] is not recognized' % netF)
-    
-    return init_net(net, gpu_ids)
     
 ############   Classes     ##############
 class BlurNet(nn.Module):
@@ -661,80 +641,6 @@ class NLayerDiscriminator(nn.Module):
 
 #     def forward(self, input):
 #         return self.model(input)
-
-# class PatchSampleF(nn.Module):
-#     def __init__(self, use_mlp=False, init_type='normal', init_gain=0.02, nc=256, gpu_ids=[]):
-#         # potential issues: currently, we use the same patch_ids for multiple images in the batch
-#         super(PatchSampleF, self).__init__()
-#         self.l2norm = Normalize(2)
-#         self.use_mlp = use_mlp
-#         self.nc = nc  # hard-coded
-#         self.mlp_init = False
-#         self.init_type = init_type
-#         self.init_gain = init_gain
-#         self.gpu_ids = gpu_ids
-
-#     def create_mlp(self, feats):
-#         for mlp_id, feat in enumerate(feats):
-#             input_nc = feat.shape[1]
-#             mlp = nn.Sequential(*[nn.Linear(input_nc, self.nc), nn.ReLU(), nn.Linear(self.nc, self.nc)])
-#             mlp.cuda()
-#             setattr(self, 'mlp_%d' % mlp_id, mlp)
-#         init_net(self, self.init_type, self.init_gain, self.gpu_ids)
-#         self.mlp_init = True
-
-#     def forward(self, feats, num_patches=64, patch_ids=None):
-#         return_ids = []
-#         return_feats = []
-#         if self.use_mlp and not self.mlp_init:
-#             self.create_mlp(feats)
-#         for feat_id, feat in enumerate(feats):
-#             B, H, W = feat.shape[0], feat.shape[2], feat.shape[3]
-#             feat_reshape = feat.permute(0, 2, 3, 1).flatten(1, 2)
-#             if num_patches > 0:
-#                 if patch_ids is not None:
-#                     patch_id = patch_ids[feat_id]
-#                 else:
-#                     patch_id = torch.randperm(feat_reshape.shape[1], device=feats[0].device)
-#                     patch_id = patch_id[:int(min(num_patches, patch_id.shape[0]))]  # .to(patch_ids.device)
-#                 x_sample = feat_reshape[:, patch_id, :].flatten(0, 1)  # reshape(-1, x.shape[1])
-#             else:
-#                 x_sample = feat_reshape
-#                 patch_id = []
-#             if self.use_mlp:
-#                 mlp = getattr(self, 'mlp_%d' % feat_id)
-#                 x_sample = mlp(x_sample)
-#             return_ids.append(patch_id)
-#             x_sample = self.l2norm(x_sample)
-
-#             if num_patches == 0:
-#                 x_sample = x_sample.permute(0, 2, 1).reshape([B, x_sample.shape[-1], H, W])
-#             return_feats.append(x_sample)
-#         return return_feats, return_ids
-
-
-class ReshapeF(nn.Module):
-    def __init__(self):
-        super(ReshapeF, self).__init__()
-        model = [nn.AdaptiveAvgPool2d(4)]
-        self.model = nn.Sequential(*model)
-        self.l2norm = Normalize(2)
-
-    def forward(self, x):
-        x = self.model(x)
-        x_reshape = x.permute(0, 2, 3, 1).flatten(0, 2)
-        return self.l2norm(x_reshape)
-
-class Normalize(nn.Module):
-
-    def __init__(self, power=2):
-        super(Normalize, self).__init__()
-        self.power = power
-
-    def forward(self, x):
-        norm = x.pow(self.power).sum(1, keepdim=True).pow(1. / self.power)
-        out = x.div(norm + 1e-7)
-        return out
 
 def pixel_reshuffle(input, upscale_factor):
     """Rearranges elements in a tensor of shape ``[*, C, H, W]`` to a
