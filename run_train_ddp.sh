cfd46270d620306921079a00de3883ffbe071e28
yjzhang96 <4591203423@qq.com>
2022-04-18 00:52:41 +1000

    train_ddp

--- run_train_ddp.sh
+++ run_train_ddp.sh
@@ -1,3 +1,9 @@
 ## single node multi-GPU
 python -m torch.distributed.launch --nproc_per_node=2 \
-        train.py --config=configs/config_test.yaml
+        train.py --config=configs/config_train.yaml
+
+
+## multi node multi GPU
+python -m torch.distributed.launch --nproc_per_node=2 \
+           --nnodes=2 --node_rank=0 --master_addr="192.168.66.228" \
+           --master_port=1234 train.py --config=configs/config_train.yaml
